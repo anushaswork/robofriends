f8246bce34ffc968a7bb5dfb2981824062955170
Anusha Srinivasan <anushaswork@gmail.com>
2020-08-26 20:46:07 +0530

    robo friends halfway through

---
+++ src/robots.js
@@ -0,0 +1,14 @@
+export const robots =[
+	{
+		id: 1,
+		name: 'Blah1',
+		username: 'Blah1',
+		email: 'blah1@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Blah2',
+		username: 'Blah2',
+		email: 'blah2@gmail.com'
+	}
+]
